c3cfa6429ccf95d28fd3048c2e33b0e05cb46f41
kusk <lpunch3corns@naver.com>
2023-02-12 21:52:28 +0900

    장치 초기화 | GPU한테 외주를 떠넘기기 위한 사전 작업
    1. 클래스 추가
     - Engine/Engine
     - Engine/CommandQueue
     - Engine/Device
     - Engine/SwapChain
     - Engine/DescriptorHeap
    
    2. 윈도우 정보 구조체 추가 in EnginePch.h
     - struct WindowInfo
     - 윈도우가 창모드인지 전체화면 모드인지, 위치와 크기는 어떤지 받아올 데이터 구조를 정의한다.
    
    3. Engine 객체 생성 in EnginePch.cpp
     - Engine 클래스를 전역으로 선언하여 어디서든 사용할 수 있게 함.
     - Singleton으로 사용하거나 간단히 전역으로 생성. (여기선 전역으로 생성)
     - EnginePch.h 에 extern 하여 사용
    
    4. Engine 내용 추가
     - 윈도우 정보 받아와서 초기화
     - 윈도우 크기 정보 받아서 변경하는 함수 추가 (ResizeWindow)

---
+++ Game/Engine/Engine.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "Engine.h"
+
+void Engine::Init(const WindowInfo& info)
+{
+	_window = info;
+	ResizeWindow(_window.width, _window.height);
+
+	// 그려질 화면 크기를 설정
+	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
+	_scissorRect = CD3DX12_RECT(0, 0, info.width, info.height);
+
+
+}
+
+void Engine::Render()
+{
+}
+
+// 실시간으로 윈도우 크기를 변경할 수 있도록 크기 인자를 받음.
+void Engine::ResizeWindow(int32 width, int32 height)
+{
+	// 윈도우 크기 창 설정
+	_window.width = width;
+	_window.height = height;
+
+	RECT rect = { 0, 0, width, height };
+	::AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+	::SetWindowPos(_window.hwnd, 0, 100, 100, width, height, 0);
+}
--- Game/Engine/EnginePch.h
+++ Game/Engine/EnginePch.h
@@ -43,4 +43,14 @@ using Vec3			= XMFLOAT3;
 using Vec4			= XMFLOAT4;
 using Matrix		= XMMATRIX;
 
+struct WindowInfo
+{
+	HWND	hwnd; // 출력 윈도우
+	int32	width; // 너비
+	int32	height; // 높이
+	bool	windowed; // 창모드 or 전체화면
+};
+
+extern unique_ptr<class Engine> GEngine;
+
 void HelloEngine();
